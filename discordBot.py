72f7071f67eaaec71a48cf60cde5bc286c2762de
puneeth.kpati <puneeth.kpati@gmail.com>
2020-07-08 21:05:13 +1000

    encode function works like a charm

--- discordBot.py
+++ discordBot.py
@@ -1,6 +1,7 @@
 
 import discord
 from discord.ext import commands
+from base_converter.base import BaseConverter
 
 token = open('token', 'r').read().strip()
 bot = commands.Bot(command_prefix='%')
@@ -9,5 +10,20 @@ bot = commands.Bot(command_prefix='%')
 async def on_ready():
 	print('Hack0r is ready...')
 
+@bot.command()
+async def encode(ctx, message=None):
+	if not message:
+		await ctx.send('Fam, what do you want me to encode?')
+		await ctx.send('```\nUsage: &encode message```')
+
+	encoder = BaseConverter()
+	encoded = encoder.encode(message)
+
+	for item in encoded:
+		res = '```\n{}\n```'.format(item)
+		print(res)
+		await ctx.send(res)
+
+	return
 
 bot.run(token)
