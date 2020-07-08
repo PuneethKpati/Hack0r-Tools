bc77599b53e9db0df9f7c891c71660c440170cb3
puneeth.kpati <puneeth.kpati@gmail.com>
2020-07-08 20:34:13 +1000

    added the basic encoding functionality

--- base_decoder/base.py
+++ base_decoder/base.py
@@ -7,7 +7,15 @@ class BaseConverter():
 		self.encoderF = {'Base64': base64.b64encode, 'Base16': base64.b16encode, 'Base32': base64.b32encode, 'Base58': base58.b58encode}
 		self.decoderF = {'Base64': base64.b64decode, 'Base16': base64.b16decode, 'Base32': base64.b32decode, 'Base58': base58.b58decode}
 
-	
+	def encode(self, message, base=None):
+		res = []
+		count = 0
+		for func in self.encoderF:
+			encodedBytes = self.encoderF[func](bytes(message, 'utf-8'))
+			encoded = str(encodedBytes, 'utf-8' )
+			res.append(func + ' : ' + encoded)
+
+		return res
 
 	def decode(self, message):
 		for func in self.decoderF:
