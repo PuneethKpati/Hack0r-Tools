1b396894aba948f5118cb96199c08100dd787695
puneeth.kpati <puneeth.kpati@gmail.com>
2020-07-19 19:37:20 +1000

    added the basic listening server skeleton

--- server_listener/server_listener.py
+++ server_listener/server_listener.py
@@ -4,19 +4,19 @@ class Listener:
 	def __init__(self, host, port):
 		self.host = host
 		self.port = port
-
+		self.listener = None
 
 	def setup(self):
-		listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		listener.bind((self.host, self.port))
-		listener.listen(5)
+		self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.listener.bind((self.host, self.port))
+		self.listener.listen(5)
 
 		return 'Server Active on {} and port {}'.format(self.host, self.port)
 
 	def run(self):
-		clientsocket, addr = listener.accept()
+		clientsocket, addr = self.listener.accept()
 		m = clientsocket.recv(2048)
 		message = str(m, 'utf-8')
-		return message
+		print( message )
